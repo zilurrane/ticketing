f8f143523249e503d1d38207bd8e5ff31b451457
zilurrane <zilurrane@gmail.com>
2021-06-21 22:30:57 +0530

    Added Signup flow with password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,22 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
+// An interface that describes the properties
+// that are requried to create a new User
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -16,10 +32,18 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
