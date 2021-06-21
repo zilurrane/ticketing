347bf7070e984e527feb14ded699e9a46ecf6f53
zilurrane <zilurrane@gmail.com>
2021-06-21 22:01:57 +0530

    Initialize MongoDB connection

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,18 +1,20 @@
-import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage('Password must be between 4 and 20 characters')
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -21,7 +23,7 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating a user...");
+    console.log('Creating a user...');
     throw new DatabaseConnectionError();
 
     res.send({});
